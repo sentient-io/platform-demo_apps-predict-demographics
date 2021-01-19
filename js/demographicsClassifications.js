b0711c221e2a7e04c7df584e91e858afae90b866
Priyadharshini <priyadharshini@sentient.io>
2021-01-19 12:58:38 +0530

    latest version

---
+++ js/demographicsClassifications.js
@@ -0,0 +1,30 @@
+/* +---------------------------------------+ */
+/* | Call Demographics Classifications API | */
+/* +---------------------------------------+ */
+demographicsClassification = (base64) => {
+	return new Promise((resolve, reject) => {
+		//console.log('Demographics Classifications API');
+
+		$.ajax({
+			method: 'POST',
+			url:
+				'https://apis.sentient.io/microservices/cv/democlass/v0/getpredictions',
+			headers: { 'x-api-key': apikey, 'Content-Type': 'application/json' },
+			data: JSON.stringify({ image_base64: base64 }),
+			success: (result) => {
+				//console.log('Result:', result.results.persons[0]);
+				if (!result.results.persons[0]) {
+					togglePopUpAlert({
+						alertTitle: 'No Face Detected',
+						alertContent:
+							'Photo must contains at least one front facing person. ',
+					});
+				}
+				resolve(result);
+			},
+			error: (err) => {
+				reject(err);
+			},
+		});
+	});
+};
